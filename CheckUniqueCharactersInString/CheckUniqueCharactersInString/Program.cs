87f4c590a65294c7eec1b1628385b0c248e0693f
ancajeckel <anca.jeckel@yahoo.com>
2018-11-30 11:42:57 +0200

    Problem 2 - Check if string has repeated chars

---
+++ CheckUniqueCharactersInString/CheckUniqueCharactersInString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CheckUniqueCharactersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            var str = Console.ReadLine();
+
+            var arr = str.ToCharArray();
+            var list = "";
+            bool has_unique = true;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (list.Contains(arr[i]))
+                {
+                    Console.WriteLine("String has repeating characters!");
+                    has_unique = false;
+                    break;
+                }
+                list = list + arr[i];
+            }
+
+            if (has_unique)
+            {
+                Console.WriteLine("String has only unique characters");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
